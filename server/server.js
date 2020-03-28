75dba28cbd7546b45cca5ebab268fe131cf47ced
Krzysztof Bogusz <krzysztofbogusz050591@gmail.com>
2020-03-28 14:57:43 +0100

    SL-2 [Moved auth to graphql]

--- server/server.js
+++ server/server.js
@@ -3,34 +3,9 @@ const fetch = require("node-fetch");
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 const schema = require('./shema')
-const keys = require('./keys')
 
 const app = express();
 app.use(cors());
-
-app.get('/auth', function (req, res) {
-  const code = req.query.code
-  const secret = keys.sec;
-  const url = new URL("https://www.strava.com/oauth/token");
-  const params = { client_id: '43111', client_secret: secret, code, grant_type: 'authorization_code'}
- 
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-
-  fetch('' + url, {
-    method: 'POST', 
-    mode: 'cors',
-  }).then(resp => {
-    return (resp.json())
-  }).then ( resps => {
-    const user = resps.athlete
-    const access_token = resps.access_token;
-    const refresh_token = resps.refresh_token
-    res.send({ access_token, refresh_token, user });
-  }).catch(err => {
-    console.log('Error while getting token', err);
-    res.send({err: 'error!'})
-  })
-});
 
 app.use('/graphql', graphqlHTTP({
   schema,
--- server/shema.js
+++ server/shema.js
@@ -1,5 +1,24 @@
 const { GraphQLObjectType, GraphQLInt, GraphQLFloat,  GraphQLString, GraphQLSchema, GraphQLList, Query, GraphQLOutputType } = require('graphql');
+const keys = require('./keys')
 const axios = require('axios');
+
+const UserModel = new GraphQLObjectType({
+  name: 'UserModel',
+  fields: () => ({
+    firstname: { type: GraphQLString },
+    lastname: { type: GraphQLString },
+    profile: { type: GraphQLString },
+  })
+})
+
+const LoginData = new GraphQLObjectType({
+  name: 'LoginData',
+  fields: () => ({
+    token: { type: GraphQLString },
+    refresh_token: { type: GraphQLString },
+    user: { type: UserModel },
+  })
+})
 
 const AthleteData = new GraphQLObjectType({
   name: 'BasicAthleteData',
@@ -49,7 +68,36 @@ const RootQuery = new GraphQLObjectType({
         axios.defaults.headers.common['Authorization'] = `Bearer ${args.token}` 
         return axios.get('https://www.strava.com/api/v3/activities')
           .then(res => res.data);
-      }
+      },
+    },
+    auth: {
+      type: LoginData,
+      args: {
+        code: {type: GraphQLString},
+      },
+      resolve(parent, args) {
+        const code = args.code;
+        const secret = keys.sec;
+        const params = { client_id: '43111', client_secret: secret, code, grant_type: 'authorization_code'}
+        const url = new URL("https://www.strava.com/oauth/token" );
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+        return axios.post('' + url, {
+          mode: 'no-cors',
+          headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        }
+      },
+        )
+          .then(res => {
+            return {
+              token: res.data.access_token,
+              refresh_token: res.data.refresh_token,
+              user: {...res.data.athlete }
+            };
+          })
+          .catch(err => console.log('Error', err.status))
+      },
     }
   }),
 })
